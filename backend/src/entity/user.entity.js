81f5bf3fb7c00273c1aed1c280a8208b2711ad3e
Cristobal <cristobalalarcon22@gmail.com>
2024-10-24 18:41:04 -0300

    fix: modifica atributos de user

--- backend/src/entity/user.entity.js
+++ backend/src/entity/user.entity.js
@@ -1,39 +1,40 @@
 "use strict";
 import { EntitySchema } from "typeorm";
 
-const UserSchema = new EntitySchema({
+export default new EntitySchema({
   name: "User",
   tableName: "users",
   columns: {
-    id: {
-      type: "int",
-      primary: true,
-      generated: true,
-    },
-    nombreCompleto: {
-      type: "varchar",
-      length: 255,
-      nullable: false,
-    },
-    rut: {
+    RUN: {
       type: "varchar",
       length: 12,
+      primary: true,
       nullable: false,
       unique: true,
     },
-    email: {
+    nombre: {
+      type: "varchar",
+      length: 255,
+      nullable: false,
+    },
+    correo: {
       type: "varchar",
       length: 255,
       nullable: false,
       unique: true,
-    },
-    rol: {
-      type: "varchar",
-      length: 50,
-      nullable: false,
     },
     password: {
       type: "varchar",
+      nullable: false,
+    },
+    direccion: {
+      type: "varchar",
+      length: 255,
+      nullable: false,
+    },
+    rol: {
+      type: "enum",
+      enum: ["alumno", "profesor", "jefeUTP", "admin"],
       nullable: false,
     },
     createdAt: {
@@ -50,21 +51,14 @@ const UserSchema = new EntitySchema({
   },
   indices: [
     {
-      name: "IDX_USER",
-      columns: ["id"],
+      name: "IDX_USER_RUN",
+      columns: ["RUN"],
       unique: true,
     },
     {
-      name: "IDX_USER_RUT",
-      columns: ["rut"],
-      unique: true,
-    },
-    {
-      name: "IDX_USER_EMAIL",
-      columns: ["email"],
+      name: "IDX_USER_correo",
+      columns: ["correo"],
       unique: true,
     },
   ],
 });
-
-export default UserSchema;
