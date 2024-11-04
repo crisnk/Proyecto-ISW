2893a78583bc36fd30a7b087345c2fb66f67a2de
Cristobal <cristobalalarcon22@gmail.com>
2024-11-04 00:36:08 -0300

    style: realiza un ajuste en la indentación

--- backend/src/middlewares/authorization.middleware.js
+++ backend/src/middlewares/authorization.middleware.js
@@ -1,40 +1,28 @@
 import User from "../entity/user.entity.js";
 import { AppDataSource } from "../config/configDb.js";
-import {
-handleErrorClient,
-handleErrorServer,
-} from "../handlers/responseHandlers.js";
+import { handleErrorClient, handleErrorServer } from "../handlers/responseHandlers.js";
 
 export function isAuthorized(...allowedRoles) {
     return async function (req, res, next) {
-try {
-    const userRepository = AppDataSource.getRepository(User);
+        try {
+            const userRepository = AppDataSource.getRepository(User);
 
-    const userFound = await userRepository.findOneBy({ email: req.user.email });
+            const userFound = await userRepository.findOneBy({ email: req.user.email });
 
-    if (!userFound) {
-    return handleErrorClient(
-        res,
-        404,
-        "Usuario no encontrado en la base de datos",
-    );
+            if (!userFound) {
+                return handleErrorClient(res, 404, "Usuario no encontrado en la base de datos");
+            }
+
+            if (!allowedRoles.includes(userFound.rol)) {
+                return handleErrorClient(res, 403,
+                    "Error al acceder al recurso",
+                    `Se requiere uno de los siguientes roles: ${allowedRoles.join(", ")} para realizar esta acción.`
+                );
+            }
+
+            next();
+        } catch (error) {
+            handleErrorServer(res, 500, error.message);
+        }
     }
-
-    if (!allowedRoles.includes(userFound.rol)) {
-        return handleErrorClient(
-          res,
-          403,
-          "Error al acceder al recurso",
-          `Se requiere uno de los siguientes roles: ${allowedRoles.join(", ")} para realizar esta acción.`
-        );
-      }
-    next();
-} catch (error) {
-    handleErrorServer(
-    res,
-    500,
-    error.message,
-    );
-}
-}
 }
