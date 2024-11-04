6570a8d153c246032b2a2baf90e034765340e7ad
Cristobal <cristobalalarcon22@gmail.com>
2024-11-04 00:34:47 -0300

    feat: protege las rutas de las practicas según el rol correspondiente

--- backend/src/routes/practica.routes.js
+++ backend/src/routes/practica.routes.js
@@ -2,15 +2,20 @@
 
 import { Router } from "express";
 import { crearPractica, obtenerPracticas, obtenerPractica, modificarPractica, eliminarPractica } from "../controllers/practica.controller.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
+import { isAuthorized } from "../middlewares/authorization.middleware.js";
 
 const router = Router();
 
+router.use(authenticateJwt);
 router
-    .post("/crear", crearPractica)
-    .get("/all", obtenerPracticas)
-    .get("/:ID_practica", obtenerPractica)
-    .put("/modificar/:ID_practica", modificarPractica)
-    .delete("/:ID_practica", eliminarPractica);
+    // Sólo EDP, jefeUTP y admin
+    .post("/crear", isAuthorized("EDP", "jefeUTP", "administrador"), crearPractica)
+    .put("/modificar/:ID_practica", isAuthorized("EDP", "jefeUTP", "administrador"), modificarPractica)
+    .delete("/:ID_practica", isAuthorized("EDP", "jefeUTP","administrador"), eliminarPractica)
 
+    // Usuarios
+    .get("/all", obtenerPracticas)
+    .get("/:ID_practica", obtenerPractica);
 
 export default router;
